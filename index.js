3a627cc703ce4d8de4593046fe90863bbc7b3d90
unknown <gespejo2144@gmail.com>
2022-03-30 19:42:35 -0700

    added tree, refactored code into a function

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let greenCharacter = document.createElement('img')
+function newImage(path, left, bottom){
+    let imgSprite = document.createElement('img')
+    imgSprite.src = path
+    imgSprite.style.position = 'fixed'
+    imgSprite.style.left = left
+    imgSprite.style.bottom = bottom
+    document.body.append(imgSprite)
+}
+
+newImage('assets/green-character.gif','100px', '100px')
+newImage('assets/pine-tree.png','450px','200px')
+
+/*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)*/

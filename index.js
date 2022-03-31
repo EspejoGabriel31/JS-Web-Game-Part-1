56de1be03ee7bf965016bc4fcca41f5d913cc4f5
unknown <gespejo2144@gmail.com>
2022-03-30 20:11:50 -0700

    added more assets

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ function newImage(path, left, bottom){
 
 newImage('assets/green-character.gif','100px', '100px')
 newImage('assets/pine-tree.png','450px','200px')
-
+newImage('assets/tree.png','200px','300px')
+newImage('assets/pillar.png','350px','100px')
+newImage('assets/crate.png','150px','200px')
+newImage('assets/well.png','500px','425px')
 /*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
